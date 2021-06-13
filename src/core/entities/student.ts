fc9719a46419697d499c2c1fd15cd8254a474722
Alison Machado <alison.sm92@gmail.com>
2021-06-13 20:40:00 -0300

    refactor: student age

--- src/core/entities/student.ts
+++ src/core/entities/student.ts
@@ -1,3 +1,4 @@
+import Age from "./age";
 import Cpf from "./cpf";
 import Name from "./name";
 
@@ -5,10 +6,12 @@ export default class Student {
     readonly name: string;
     readonly cpf: string;
     readonly birthDate: Date;
+    readonly age: Number;
 
     constructor(name: string, cpf: string, birthDate: string) {
         this.name = new Name(name).value;
         this.cpf = new Cpf(cpf).value;
         this.birthDate = new Date(birthDate);
+        this.age = new Age(this.birthDate).value;
     }
 }
