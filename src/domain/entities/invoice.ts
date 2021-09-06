a64d7d15ff298e56a4a58db7b1517ed054ad8009
Alison Machado <alison.sm92@gmail.com>
2021-09-05 21:17:31 -0300

    feat: Add pay method

--- src/domain/entities/invoice.ts
+++ src/domain/entities/invoice.ts
@@ -1,8 +1,14 @@
+export enum InvoiceStatus {
+    PENDING = 'pending',
+    PAID = 'paid'
+}
+
 export default class Invoice {
     readonly code: string;
     readonly month: number;
     readonly year: number;
     readonly amount: number;
+    status: InvoiceStatus;
 
     constructor({ code, month, year, amount }: 
         { code: string, month: number, year: number, amount: number }) {
@@ -10,5 +16,11 @@ export default class Invoice {
         this.month = month;
         this.year = year;
         this.amount = amount;
+        this.status = InvoiceStatus.PENDING;
+    }
+
+    pay(amount: number) {
+        if(amount !== this.amount)  throw new Error('Pay amount is not equals to invoice amount');
+        this.status = InvoiceStatus.PAID;
     }
 }
