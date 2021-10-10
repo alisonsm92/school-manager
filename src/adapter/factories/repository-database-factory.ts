d2195ff52febe60e58cc19226c13b93c3e831e82
Alison Machado <alison.sm92@gmail.com>
2021-10-10 12:50:52 -0300

    refactor: rename directory

--- src/adapter/factories/repository-database-factory.ts
+++ src/adapter/factories/repository-database-factory.ts
@@ -1,8 +1,8 @@
 import RepositoryAbstractFactory from "../../domain/factories/repository-abstract-factory";
 import EnrollmentRepositoryDatabase from "../repositories/database/enrollment-repository-database";
-import ClassroomRepositoryInMemorySingleton from "../repositories/in-memory/classroom-repository-in-memory-singleton";
-import LevelRepositoryInMemorySingleton from "../repositories/in-memory/level-repository-in-memory-singleton";
-import ModuleRepositoryInMemorySingleton from "../repositories/in-memory/module-repository-in-memory-singleton";
+import ClassroomRepositoryInMemorySingleton from "../repositories/memory/classroom-repository-in-memory-singleton";
+import LevelRepositoryInMemorySingleton from "../repositories/memory/level-repository-in-memory-singleton";
+import ModuleRepositoryInMemorySingleton from "../repositories/memory/module-repository-in-memory-singleton";
 
 export default class RepositoryDatabaseFactory implements RepositoryAbstractFactory {
 
