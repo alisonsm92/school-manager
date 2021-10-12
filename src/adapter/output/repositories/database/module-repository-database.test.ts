d2027b9e48a9e0913fb7f7455ec71ffef5ddeca8
Alison Machado <alison.sm92@gmail.com>
2021-10-12 17:24:50 -0300

    feat: add module repository database

---
+++ src/adapter/output/repositories/database/module-repository-database.test.ts
@@ -0,0 +1,54 @@
+import Module from '../../../../domain/entities/module';
+import postgresql from '../../../../infra/postgresql';
+import ModuleRepositoryDatabase from './module-repository-database';
+
+const inputData: Module = {
+    level: 'EM',
+    code: '1',
+    description: '1o Ano',
+    minimumAge: 15,
+    price: 17000
+};
+
+let sut: ModuleRepositoryDatabase;
+
+beforeEach(() => {
+    sut = new ModuleRepositoryDatabase();
+});
+
+afterEach(async () => {
+    await sut.clean();
+});
+
+afterAll(async () => {
+    await postgresql.end();
+});
+
+describe('Testing ModuleRepositoryDatabase', () => {
+    describe('Find and Add method', () => {
+        test('Should return the module with code provided when it exists', async () => {
+            await sut.add(inputData);
+            const module = await sut.find('EM', '1');
+            expect(module).toHaveProperty('code', '1');
+            expect(module).toHaveProperty('level', 'EM');
+            expect(module).toHaveProperty('description', '1o Ano');
+            expect(module).toHaveProperty('minimumAge', 15);
+            expect(module).toHaveProperty('price', 17000);
+        });
+
+        test('Should return undefined when the module it is not exists', async () => {
+            await sut.add(inputData);
+            const module = await sut.find('EM', '2');
+            expect(module).toBeUndefined();
+        });
+    });
+
+    describe('Clean method', () => {
+        test('Should not found the register added', async () => {
+            await sut.add(new Module(inputData));
+            await sut.clean();
+            const module = await sut.find('EM', '1');
+            expect(module).toBeUndefined();
+        });
+    });
+});
---
+++ src/adapter/output/repositories/database/module-repository-database.ts
@@ -0,0 +1,51 @@
+import Module from '../../../../domain/entities/module';
+import ModuleRepository from '../../../../domain/repositories/module-repository';
+import postgreSQL from "../../../../infra/postgresql";
+
+type ModuleRegister = {
+    code: string,
+    level: string,
+    description: string,
+    minimum_age: number,
+    price: number
+}
+
+export default class ModuleRepositoryDatabase implements ModuleRepository {
+    private database: typeof postgreSQL;
+
+    constructor() {
+        this.database = postgreSQL;
+    }
+
+    async find(level: string, code: string) {
+        const [row] = await this.database.query<ModuleRegister>(`
+            SELECT * 
+            FROM system.module
+            WHERE level = $1
+            AND code = $2`, [level, code]);
+        return row ? new Module({
+            code: row.code,
+            level: row.level,
+            description: row.description,
+            minimumAge: row.minimum_age,
+            price: row.price
+        }) : undefined;
+    }
+
+    async add(module: Module) {
+        await this.database.query(`
+            INSERT INTO system.module (code, level, description, minimum_age, price)
+            VALUES ($1, $2, $3, $4, $5);
+        `, [
+            module.code,
+            module.level,
+            module.description,
+            module.minimumAge,
+            module.price
+        ]);
+    }
+
+    async clean() {
+        await this.database.query('DELETE FROM system.module');
+    }
+}
