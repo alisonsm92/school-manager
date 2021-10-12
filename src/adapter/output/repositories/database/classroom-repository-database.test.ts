542b2fb9932565b69620724c31c414e3c5e4e284
Alison Machado <alison.sm92@gmail.com>
2021-10-12 16:49:59 -0300

    feat: add classroom repository database

---
+++ src/adapter/output/repositories/database/classroom-repository-database.test.ts
@@ -0,0 +1,70 @@
+import Classroom from '../../../../domain/entities/classroom';
+import postgresql from '../../../../infra/postgresql';
+import ClassroomRepositoryDatabase from './classroom-repository-database';
+
+const inputData = {
+    level: "EM",
+    module: "1",
+    code: "A",
+    capacity: 10,
+    startDate: new Date('2021-05-01'),
+    endDate: new Date('2021-06-30')
+};
+
+let sut: ClassroomRepositoryDatabase;
+
+beforeEach(() => {
+    sut = new ClassroomRepositoryDatabase();
+});
+
+afterEach(async () => {
+    await sut.clean();
+});
+
+afterAll(async () => {
+    await postgresql.end();
+});
+
+describe('Testing ClassRepositoryDatabase', () => {
+    describe('Find and Add method', () => {
+        test('Should return the classroom with code provided when it exists', async () => {
+            await sut.add(new Classroom(inputData));
+            const classroom = await sut.find('EM', '1', 'A');
+            expect(classroom).toHaveProperty('code', 'A');
+            expect(classroom).toHaveProperty('level', 'EM');
+            expect(classroom).toHaveProperty('module', '1');
+            expect(classroom).toHaveProperty('capacity', 10);
+            expect(classroom).toHaveProperty('startDate', new Date('2021-05-01'));
+            expect(classroom).toHaveProperty('endDate', new Date('2021-06-30'));
+        });
+
+        test('Should return undefined when the classroom it is not exists', async () => {
+            await sut.add(new Classroom(inputData));
+            const classroom = await sut.find('EM', '2', 'B');
+            expect(classroom).toBeUndefined();
+        });
+    });
+
+    describe('Update method', () => {
+        test('Should update the capacity with the number provided', async () => {
+            await sut.add(new Classroom(inputData));
+            await sut.update(new Classroom({ ...inputData, capacity: 11 }));
+            const classroom = await sut.find('EM', '1', 'A');
+            expect(classroom).toHaveProperty('code', 'A');
+            expect(classroom).toHaveProperty('level', 'EM');
+            expect(classroom).toHaveProperty('module', '1');
+            expect(classroom).toHaveProperty('capacity', 11);
+            expect(classroom).toHaveProperty('startDate', new Date('2021-05-01'));
+            expect(classroom).toHaveProperty('endDate', new Date('2021-06-30'));
+        });
+    });
+
+    describe('Clean method', () => {
+        test('Should not found the register added', async () => {
+            await sut.add(new Classroom(inputData));
+            await sut.clean();
+            const classroom = await sut.find('EM', '1', 'A');
+            expect(classroom).toBeUndefined();
+        });
+    });
+});
---
+++ src/adapter/output/repositories/database/classroom-repository-database.ts
@@ -0,0 +1,72 @@
+import Classroom from '../../../../domain/entities/classroom';
+import ClassroomRepository from '../../../../domain/repositories/classroom-repository';
+import postgreSQL from "../../../../infra/postgresql";
+
+type ClassroomRegister = {
+    code: string,
+    level: string,
+    module: string,
+    capacity: number,
+    start_date: string,
+    end_date: string,
+}
+
+export default class ClassroomRepositoryDatabase implements ClassroomRepository {
+    private database: typeof postgreSQL;
+
+    constructor() {
+        this.database = postgreSQL;
+    }
+
+    async find(level: string, module: string, code: string) {
+        const [row] = await this.database.query<ClassroomRegister>(`
+            SELECT * 
+            FROM system.classroom
+            WHERE level = $1
+            AND module = $2
+            AND code = $3`, [level, module, code]);
+        return row ? new Classroom({
+            code: row.code,
+            level: row.level,
+            module: row.module,
+            capacity: row.capacity,
+            startDate: new Date(row.start_date),
+            endDate: new Date(row.end_date)
+        }) : undefined;
+    }
+
+    async add(classroom: Classroom) {
+        await this.database.query(`
+            INSERT INTO system.classroom (code, level, module, capacity, start_date, end_date)
+            VALUES ($1, $2, $3, $4, $5, $6);
+        `, [
+            classroom.code,
+            classroom.level,
+            classroom.module,
+            classroom.capacity,
+            classroom.startDate,
+            classroom.endDate
+        ]);
+    }
+
+    async update(classroom: Classroom) {
+        await this.database.query(`
+            UPDATE system.classroom
+            SET capacity = $4, start_date = $5, end_date = $6
+            WHERE code = $1
+            AND level = $2
+            AND module = $3
+        `, [
+            classroom.code,
+            classroom.level,
+            classroom.module,
+            classroom.capacity,
+            classroom.startDate,
+            classroom.endDate
+        ]);
+    }
+
+    async clean() {
+        await this.database.query('DELETE FROM system.classroom');
+    }
+}
