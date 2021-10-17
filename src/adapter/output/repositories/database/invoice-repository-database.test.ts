020708c6ea7cc4f00ceb3336503d2697cf26e693
Alison Machado <alison.sm92@gmail.com>
2021-10-17 20:43:20 -0300

    test: add test case to findMany method

--- src/adapter/output/repositories/database/invoice-repository-database.test.ts
+++ src/adapter/output/repositories/database/invoice-repository-database.test.ts
@@ -25,7 +25,7 @@ afterAll(async () => {
 
 describe('Testing InvoiceRepositoryDatabase', () => {
     describe('Find and Add method', () => {
-        test('Should return the invoice with code provided when it exists', async () => {
+        test('Should return the invoice with code, month and year provided when it exists', async () => {
             await sut.add(new Invoice(inputData));
             const invoice = await sut.find(inputData.code, inputData.month, inputData.year);
             expect(invoice).toHaveProperty('code', inputData.code);
@@ -37,6 +37,21 @@ describe('Testing InvoiceRepositoryDatabase', () => {
         test('Should return undefined when the invoice it is not exists', async () => {
             const invoice = await sut.find(inputData.code, inputData.month, inputData.year);
             expect(invoice).toBeUndefined();
+        });
+    });
+
+    describe('FindMany method', () => {
+        test('Should return the invoices with code provided when it exists', async () => {
+            const inputInvoices = [new Invoice(inputData), new Invoice({ ...inputData, month: 8 })];
+            await sut.add(inputInvoices[0]);
+            await sut.add(inputInvoices[1]);
+            const invoices = await sut.findMany(inputData.code);
+            expect(invoices).toEqual(inputInvoices);
+        });
+
+        test('Should return undefined when not exists invoices with the code', async () => {
+            const invoices = await sut.findMany(inputData.code);
+            expect(invoices).toHaveLength(0);
         });
     });
 
