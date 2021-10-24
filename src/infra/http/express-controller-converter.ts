b5849a0e5955863deb98f4113a3c53d2180fa16a
Alison Machado <alison.sm92@gmail.com>
2021-10-23 21:17:37 -0300

    feat: add express controller converter

---
+++ src/infra/http/express-controller-converter.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import Controller from '../../adapter/input/controllers/controller';
+
+export default class ExpressControllerConverter {
+    private readonly controller: Controller;
+
+    constructor(controller: Controller) {
+        this.controller = controller;
+    }
+
+    get handler() {
+        const controller = this.controller;
+        return async function (req: Request, res: Response) {
+            try {
+                const { params, body, headers } = req;
+                const response = await controller.handle({ params, body, headers });
+                res.json(response);
+            } catch (e) {
+                if(e instanceof Error) {
+                    res.status(500);
+                    res.json({ message: e.message });
+                }
+                throw e;
+            }
+        }
+    }
+}
