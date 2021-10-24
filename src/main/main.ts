b4ec2b97d61df215718168275bc938c09b9e0600
Alison Machado <alison.sm92@gmail.com>
2021-10-23 21:18:56 -0300

    feat: add main file

---
+++ src/main/main.ts
@@ -0,0 +1,6 @@
+import RepositoryDatabaseFactory from "../adapter/output/factories/repository-database-factory";
+import HttpServer from "../infra/http/http-server";
+
+const repositoryFactory = new RepositoryDatabaseFactory();
+const httpServer = new HttpServer(repositoryFactory);
+httpServer.start();
