9b395606513848f9515370ae25518bf66aee741f
Alison Machado <alison.sm92@gmail.com>
2021-10-23 21:14:22 -0300

    build: add server config

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,6 +2,9 @@ import { config } from 'dotenv';
 config();
 
 export default {
+    server: {
+        port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    },
     postgres: {
         host: process.env.POSTGRES_HOST || 'localhost',
         database: process.env.POSTGRES_DATABASE || 'default',
