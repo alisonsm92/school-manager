baff8ff7bac00b8337261582723786dba0f027c0
Alison Machado <alison.sm92@gmail.com>
2021-10-30 17:29:48 -0300

    feat: add enrollment payments route

--- src/infra/http/router.ts
+++ src/infra/http/router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import EnrollStudentController from "../../adapter/input/controllers/enroll-student-controller";
 import GetEnrollmentController from "../../adapter/input/controllers/get-enrollment-controller";
+import PayInvoiceController from "../../adapter/input/controllers/pay-invoice-controller";
 import RepositoryAbstractFactory from "../../domain/factories/repository-abstract-factory";
 import ExpressConverter from "./express-controller-converter";
 
@@ -11,6 +12,8 @@ export default class Router {
             new GetEnrollmentController(repositoryFactory)).handler);
         router.post('/enrollments', new ExpressConverter(
             new EnrollStudentController(repositoryFactory)).handler);
+        router.post('/enrollments/:code/payments', new ExpressConverter(
+            new PayInvoiceController(repositoryFactory)).handler);
         return router;
     }
 }
