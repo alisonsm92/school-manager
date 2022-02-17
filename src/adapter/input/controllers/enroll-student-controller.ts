b61e04f5b56bcac6ec24e23aea3d67305ec4a4ae
Alison Machado <alison.sm92@gmail.com>
2022-02-17 19:46:01 -0300

    feat: error handling on enroll student flow

--- src/adapter/input/controllers/enroll-student-controller.ts
+++ src/adapter/input/controllers/enroll-student-controller.ts
@@ -1,7 +1,10 @@
 import EnrollStudentInputData from '../../../domain/data/enroll-student-input-data'
 import EnrollStudentOutputData from '../../../domain/data/enroll-student-output-data'
+import InvalidInputError from '../../../domain/errors/invalid-input'
+import ResourceNotFound from '../../../domain/errors/resource-not-found'
 import RepositoryAbstractFactory from '../../../domain/factories/repository-abstract-factory'
 import EnrollStudent from '../../../domain/usecases/enroll-student'
+import BadRequest from '../../output/http/bad-request'
 import { HttpRequest } from '../http/http-request'
 import Controller from './controller'
 
@@ -27,9 +30,16 @@ export default class EnrollStudentController implements Controller {
     }
 
     async handle (httpRequest: EnrollStudentRequest): Promise<EnrollStudentOutputData> {
-      const inputData = new EnrollStudentInputData(httpRequest.body)
-      const enrollStudent = new EnrollStudent(this.repositoryFactory)
-      const enrollStudentOutputData = await enrollStudent.execute(inputData)
-      return enrollStudentOutputData
+      try {
+        const inputData = new EnrollStudentInputData(httpRequest.body)
+        const enrollStudent = new EnrollStudent(this.repositoryFactory)
+        const enrollStudentOutputData = await enrollStudent.execute(inputData)
+        return enrollStudentOutputData
+      } catch (e: unknown) {
+        if (e instanceof ResourceNotFound || e instanceof InvalidInputError) {
+          throw new BadRequest(e.message)
+        }
+        throw e
+      }
     }
 }
--- src/domain/usecases/enroll-student.ts
+++ src/domain/usecases/enroll-student.ts
@@ -9,6 +9,8 @@ import RepositoryAbstractFactory from '../factories/repository-abstract-factory'
 import ClassroomRepository from '../repositories/classroom-repository'
 import EnrollmentRepository from '../repositories/enrollment-repository'
 import ModuleRepository from '../repositories/module-repository'
+import ResourceNotFound from '../errors/resource-not-found'
+import InvalidInputError from '../errors/invalid-input'
 
 export default class EnrollStudent {
     private readonly enrollmentRepository: EnrollmentRepository;
@@ -31,17 +33,19 @@ export default class EnrollStudent {
           birthDate: inputData.student.birthDate
         })
         const level = await this.levelRepository.find(inputData.level)
-        if (!level) throw new Error('Level not found')
+        if (!level) throw new ResourceNotFound('Level')
         const module = await this.moduleRepository.find(inputData.level, inputData.module)
-        if (!module) throw new Error('Module not found')
+        if (!module) throw new ResourceNotFound('Module')
         const classroom = await this.classroomRepository.find(
           inputData.level, inputData.module, inputData.classroom
         )
-        if (!classroom) throw new Error('Classroom not found')
+        if (!classroom) throw new ResourceNotFound('Classroom')
         const studentsEnrolled = await this.enrollmentRepository.findAllByClass(classroom)
-        if (classroom.capacity === studentsEnrolled.length) throw new Error('Class is over capacity')
+        if (classroom.capacity === studentsEnrolled.length) {
+          throw new InvalidInputError('Class is over capacity')
+        }
         if (await this.enrollmentRepository.findByCpf(inputData.student.cpf)) {
-          throw new Error('Enrollment with duplicated student is not allowed')
+          throw new InvalidInputError('Enrollment with duplicated student is not allowed')
         }
         const sequence = await this.enrollmentRepository.count() + 1
         const issueDate = new Date()
@@ -52,8 +56,8 @@ export default class EnrollStudent {
         await this.enrollmentRepository.add(enrollment)
         return new EnrollStudentOutputData(enrollment)
       } catch (error) {
-        if (error instanceof InvalidCpfError) throw new Error('Invalid student cpf')
-        if (error instanceof InvalidNameError) throw new Error('Invalid student name')
+        if (error instanceof InvalidCpfError) throw new InvalidInputError('Invalid student cpf')
+        if (error instanceof InvalidNameError) throw new InvalidInputError('Invalid student name')
         throw error
       }
     }
