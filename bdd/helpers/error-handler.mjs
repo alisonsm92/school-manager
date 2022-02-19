49d5a300cc71465a3736913d4cf84c3aa6c362f2
Alison Machado <alison.sm92@gmail.com>
2022-02-19 16:01:25 -0300

    test: improve error message

--- bdd/helpers/error-handler.mjs
+++ bdd/helpers/error-handler.mjs
@@ -1,6 +1,6 @@
 function formatErrorMessage (error) {
   return `
-      ${error.code}
+      ${error.code || error.response?.statusText}
       Request: ${error.config?.method.toUpperCase()} ${error.config?.url} ${error.config?.data}
       Response: ${error.response?.status} ${JSON.stringify(error.response?.data)}
       `
