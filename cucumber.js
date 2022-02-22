63a3c9f2c5f391fd39f7e6f99d4f4540a13ab21b
Alison Machado <alison.sm92@gmail.com>
2022-02-22 16:30:16 -0300

    test: specify a theme for cucumber report

--- cucumber.js
+++ cucumber.js
@@ -4,6 +4,7 @@ module.exports = {
     --require-module ts-node/register
     --format @cucumber/pretty-formatter
     --format html:./src/test/bdd/report/bdd-report.html
+    --format-options '{"theme":{"feature keyword":["magenta","bold"],"scenario keyword":["magenta","bold"],"step keyword":["bold"], "location": ["grey"], "datatable": ["grey"]}}'
     --publish-quiet
     `
 }
