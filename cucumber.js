fb2b87e8653687b9e0bf7de1c24070925b733ce3
Alison Machado <alison.sm92@gmail.com>
2022-02-22 16:07:10 -0300

    test: change cucumber formatter

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,7 @@ module.exports = {
   default: `
     --require ./src/test/bdd/step-definitions/**/*.ts
     --require-module ts-node/register
-    --format cucumber-console-formatter
+    --format @cucumber/pretty-formatter
     --format html:./src/test/bdd/report/bdd-report.html
     --publish-quiet
     `
